f43a592a88bd8ce2c7c7c8fb9dd2bbeac7a144f0
TheKnorx <the.knorx@proton.me>
2026-09-06 23:20:46 +0200

    Removed preservation of edi and esi register as assembly functions also follow the System V ABI convention

--- src/x86_64/bootloader/enable_A20_line.asm
+++ src/x86_64/bootloader/enable_A20_line.asm
@@ -25,17 +25,11 @@ bits 32
 ; Returns eax = 1 if the A20 line is set; 0 otherwise
 global is_A20_on
 is_A20_on:
-    push    edi
-    push    esi
-
     mov     edi,0x112345    ; odd megabyte address.
     mov     esi,0x012345    ; even megabyte address.
     mov     [esi],esi       ; making sure that both addresses contain different values.
     mov     [edi],edi       ; (if A20 line is cleared the two pointers would point to the address 0x012345 that would contain 0x112345 (edi))
     cmp     edi, esi        ; compare addresses to see if the're equivalent.
-
-    pop     esi
-    pop     edi
 
     jne     A20_on          ; if not equivalent, A20 line is set.
     jmp     A20_off         ; if equivalent, the A20 line is cleared.
