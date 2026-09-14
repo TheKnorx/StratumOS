ff2ef51df03b598be3770c182e87bba9f85896a9
TheKnorx <the.knorx@proton.me>
2026-09-14 21:11:46 +0200

    File for the 64 bit bootloader code, which in the end loads the C kernel

---
+++ src/x86_64/bootloader/bootloader64.asm
@@ -0,0 +1,33 @@
+%define VGA_BUFFER      0xB8000
+%define COLOR_FORMAT    0x1f
+
+bits 64
+
+section .text
+
+global CODE64
+CODE64:
+    mov     rdi, hello_str
+    call    print_str
+
+; print a null-termianted string, whos pointer is located in edi, to the VGA buffer
+; void print_str(char* str);
+print_str:
+    mov     rsi, rdi
+    mov     rdi, VGA_BUFFER
+
+    .move_chars_to_vga:
+        mov     al, [rsi]
+        inc     rsi
+        test    al, al
+        jz      .done
+        mov     [rdi], al
+        inc     rdi
+        mov     byte [rdi], COLOR_FORMAT
+        inc     rdi
+        jmp     .move_chars_to_vga
+    .done:
+        ret
+
+section .rodata
+hello_str: db "Hello from long mode!", 0x00
