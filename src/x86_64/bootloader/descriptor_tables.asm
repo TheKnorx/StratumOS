8277005bc7ecdc955aa50ed86c2a418b0a5d6d12
TheKnorx <the.knorx@proton.me>
2026-09-15 11:22:57 +0200

    moved magic numbers into labels for better overview

--- src/x86_64/bootloader/descriptor_tables.asm
+++ src/x86_64/bootloader/descriptor_tables.asm
@@ -13,6 +13,15 @@
 ; ├── kernel data
 ; ├── user code
 ; └── user data
+;
+; A limit of 4 GiB is needed, cause the processor does a last check
+; before it jumps to long mode. Having a limit of 0
+; will cause a General-Protection Fault
+
+; Macro for unsetting/clearing a flag by xor'ing it thogether
+%macro NOT 1
+    (%1 ^ %1)
+%endmacro
 
 GDTR_odd_align:
     ; The intel manual says that the pseudeo-descriptor
@@ -31,58 +40,47 @@ GDT:
     ; CODE Segment
     global .Code
     .Code:          equ $ - GDT
-    .Code.limit1_lo:dw  0xFFFF  ; Is needed cause the processor does a last check
-                                ; before it jumps to long mode. Having a limit of 0
-                                ; will cause a General-Protection Fault
+    .Code.limit1_lo:dw  0xFFFF ; Set the limit to 4GiB
     .Code.base1_lo: dw  0x00   ; this is ignored in 64 bit
     .Code.base2_mid:db  0x00   ; this is ignored in 64 bit
-
-    ; Set the bits of the Access Byte accordingly
-    ; Set the P flag to indicate its present
-    ; Clear the DPL flag to indicate the highest privilege level (for now)
-    ; Set the S flag to indicate its a code segment
-    ; Set the E (executable) bit to indicate its executable
-    ; Set the DC (conforming) bit to allow execution of this code from every privilege level (for now)
-    ; Set the RW (readable/writable) bit to read only
-    ; Set the A (accessed) bit
-    .Code.access_b: db  (1<<7) || (0<<5) || (1<<4) || (1<<3) || (1<<2) || (1<<1) || (1<<0)
-
-    ; Higher 3 bits are flags, lower ones are the higher limit
-    ; Set the G (granularity) bit (- this is ignored in 64 bit)
-    ; Clear the DB (default operation size)
-    ; Set the L (long mode) flag to indicate its a descriptor for a 64 bit code segment
-    ; Also set the limit to all 1s
-    .Code.flags_lim:db  (1<<7) || (0<<6) || (1<<5) || 0x0F ; the 4th bit is reserved
-
+    ; Set the bits of the Access Byte accordingly (for description see below)
+    .Code.access_b: db  PRESENT || PRIVILEGE || CODE_SEG || EXECUTABLE || DC_CONFORM || READ_O || ACCESSED
+    ; Higher 3 bits are flags, lower ones are the higher limit (for description see below)
+    .Code.flags_lim:db  GRAN_4k || CLEARED_SZ || LONG_MODE || 0x0F ; the 4th bit is reserved
     .Code.base_hi:  db  0x00    ; this is ignored in 64 bit
-
 
     ; DATA segment
     global .Data
     .Data:          equ $ - GDT
-    .Data.limit_lo: dw  0xFFFF  ; Is needed cause the processor does a last check
-                                ; before it jumps to long mode. Having a limit of 0
-                                ; will cause a General-Protection Fault
+    .Data.limit_lo: dw  0xFFFF  ; Set the limit to 4 GiB
     .Data.base_lo:  dw  0x00    ; this is ignored in 64 bit
     .Data.base2:    db  0x00    ; this is ignored in 64 bit
-
-    ; Set the bits of the Access Byte accordingly
-    ; Set the P flag to indicate its present
-    ; Clear the DPL flag to indicate the highest privilege level (for now)
-    ; Set the S flag to indicate its a data segment
-    ; Clear the E (executable) bit to indicate its not executable, i.g. a data segment
-    ; Set the DC (direction) bit, leading to the data segment to grow upwards
-    ; Set the RW (readable/writable) bit to read+write
-    ; Set the A (accessed) bit
-    .Data.access_b: db  (1<<7) || (0<<5) || (1<<4) || (0<<3) || (0<<2) || (1<<1) || (1<<0)
-
+    ; Set the bits of the Access Byte accordingly (for description see below)
+    .Data.access_b: db  PRESENT || PRIVILEGE || DATA_SEG || NOT(EXECUTABLE) || DC_DIREC || READ_WRITE || ACCESSED)
     ; Higher 3 bits are flags, lower ones are the higher limit
     ; Set the G (granularity) bit (- this is ignored in 64 bit)
     ; Set the DB (default operation size) to 32bit
     ; Set the L (long mode) flag to indicate its a descriptor for a 64 bit code segment
-    .Data.flags_lim:db  (0<<7) || (1<<6) || (1<<5) || 0x0F   ; the 4th bit is reserved
-
+    .Data.flags_lim:db  GRAN_4k || CLEARED_SZ || LONG_MODE || 0x0F   ; the 4th bit is reserved
     .Data.base_hi:  db  0x00    ; this is ignored in 64 bit
 
 align 4  ; padding for the GDT_END pointer
 GDT_END:
+
+section .rodata
+; Access bits:
+PRESENT:    equ (1<<7)      ; Set the P flag to indicate its present
+PRIVILEGE:  equ (0<<5)      ; Clear the DPL flag to indicate the highest privilege level (for now)
+CODE_SEG:   equ (1<<4)      ; Set the S flag to indicate its a code segment, not a system segment
+DATA_SEG:   equ (1<<4)      ; Set the S flag to indicate its a data segment, not a system segment
+EXECUTABLE: equ (1<<3)      ; Set the E (executable) bit to indicate its executable
+DC_CONFORM: equ (1<<2)      ; Set the DC (conforming) bit to allow execution of this code from every privilege level (for now)
+DC_DIREC:   equ (0<<2)      ; Clear the DC (direction) bit, leading the data segment to grow upwards
+READ_O:     equ (1<<1)      ; Set the RW (readable/writable) bit to read only for code segment; write is always not allowed
+READ_WRITE: equ (1<<1)      ; Set the RW (readable/writable) bit to write for data segment; read is always allowed
+ACCESSED:   equ (1<<0)      ; Set the A (accessed) bit
+
+; Flags bits:
+GRAN_4k:    equ (1<7)       ; Set the G (granularity) bit ==> the Limit is in 4 KiB blocks (page granularity)
+CLEARED_SZ: equ (0<6)       ; Clear it cause it should be clear if the LongMode bit is set (->wiki.osdev.org)
+LONG_MODE:  equ (1<<5)      ; Set the L (long mode) flag to indicate its a descriptor for a 64 bit code segment
