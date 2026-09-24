32c277d79ab3d1390efea06e81d22ca6b2f366bd
TheKnorx <the.knorx@proton.me>
2026-09-24 13:19:10 +0200

    wrong conversion from bytes to MiB

--- src/x86_64/bootloader/enable_long_mode.asm
+++ src/x86_64/bootloader/enable_long_mode.asm
@@ -161,7 +161,7 @@ setupPaging64_16GiB:
     ;
     ; So the overall size of the whole PML4 Table is:
     ; (SIZEOF_PT_ENTRY * ENTRIES_PER_PT)(1 + 1 + 16 + 8,192) = (SIZEOF_PAGE_TABLE) * 8,210
-    ;                                                        = 8 * 512 * 8,210 = 33,628,160 = 33.62816 MiB
+    ;                                                        = 8 * 512 * 8,210 = 33,628,160 = 32.07 MiB
     ;
     ; In this function, esi and edi have the following roles:
     ; Source Index (esi): Points to the table (-entry) that will be filled
